ea7fc667feffd403b07940b73696a9a63520c41c
cvivis <hsy0754@naver.com>
2022-10-27 14:36:01 +0900

    put ResponseEntity 적용

---
+++ hello/src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,44 @@
+package com.springboot.hello.controller;
+
+
+import com.springboot.hello.controller.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/put-api")
+public class PutController {
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "Hello Post API";
+    }
+
+    @PostMapping("/data")
+    public String postMember(@RequestBody Map<String,Object> postData){
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(
+                (map) -> {
+                    sb.append(map.getKey()+" : "+map.getValue()+"\n");
+                }
+        );
+        return sb.toString();
+    }
+    //{
+    //  "name":"cvivis",
+    //  "email":"cvivis@gmail.com",
+    //  "organization":"naver"
+    //}
+    @PostMapping("/dto")
+    public String postMemberDto(@RequestBody MemberDto memberDto){
+
+        return memberDto.toString();
+    }
+
+    @PutMapping("/dto")
+    public ResponseEntity<MemberDto> putMemberDto(@RequestBody MemberDto memberDto){
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDto);
+    }
+}
