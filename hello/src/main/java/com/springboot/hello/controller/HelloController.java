e7e5fd14407bb5d1bc94d32ab400de6e2a05c328
cvivis <hsy0754@naver.com>
2022-10-27 11:53:02 +0900

    RequestParam Map으로 받기

--- hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -2,13 +2,19 @@ package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
-//    public String getVariable2(@PathVariable("variable") String var){
-//        return var;
-//    }
+
     @GetMapping(value = "/request1")
     public String getRequestParam(@RequestParam String name, @RequestParam String email, @RequestParam String password){
         return String.format("%s, %s, %s", name, email, password);
+    }
+    @GetMapping(value = "/request2")
+    public String getRequest2Param(@RequestParam Map<String,String> param){
+        param.entrySet().forEach((map)->System.out.printf("key: %s, value: %s\n",map.getKey(),map.getValue()));
+
+        return "Reqest2 finish";
     }
 }
